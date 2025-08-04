461e2a6cfe7e868042c90e7daf861fd366550c7c
luisHidalgo-git <hidalgovazquezluisangel2004@gmail.com>
2025-08-04 17:37:05 -0600

    lista amigos

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+const ulLista = document.getElementById("listaAmigos");
+const ulResultado = document.getElementById("resultado");
+
+function agregarAmigo() {
+    const input = document.getElementById("amigo");
+    const nombre = input.value.trim();
+
+    if (nombre === "") {
+        alert("Por favor, inserte un nombre.");
+        return;
+    }
+
+    amigos.push(nombre);
+    mostrarLista();
+    input.value = "";
+    input.focus();
+    ulResultado.innerHTML = "";
+}
